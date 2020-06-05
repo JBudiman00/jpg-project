6ab75bab37f6c9b9cbc74c884baa7b5348005388
Nathan Budiman <jonathan@radialogica.com>
2020-06-05 13:57:55 -0500

    Basic GUI and image uploading available

--- ScanReduceReturn/ScanReduceReturn/Form1.Designer.cs
+++ ScanReduceReturn/ScanReduceReturn/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pbView = new System.Windows.Forms.PictureBox();
+            this.btnInsertFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbView
+            // 
+            this.pbView.Location = new System.Drawing.Point(12, 12);
+            this.pbView.Name = "pbView";
+            this.pbView.Size = new System.Drawing.Size(760, 558);
+            this.pbView.TabIndex = 0;
+            this.pbView.TabStop = false;
+            this.pbView.Click += new System.EventHandler(this.pbView_Click);
+            // 
+            // btnInsertFile
+            // 
+            this.btnInsertFile.Location = new System.Drawing.Point(891, 12);
+            this.btnInsertFile.Name = "btnInsertFile";
+            this.btnInsertFile.Size = new System.Drawing.Size(75, 23);
+            this.btnInsertFile.TabIndex = 1;
+            this.btnInsertFile.Text = "Insert File";
+            this.btnInsertFile.UseVisualStyleBackColor = true;
+            this.btnInsertFile.Click += new System.EventHandler(this.btnInsertFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1061, 582);
+            this.Controls.Add(this.btnInsertFile);
+            this.Controls.Add(this.pbView);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbView)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbView;
+        private System.Windows.Forms.Button btnInsertFile;
     }
 }
 
--- ScanReduceReturn/ScanReduceReturn/Form1.cs
+++ ScanReduceReturn/ScanReduceReturn/Form1.cs
@@ -12,9 +12,28 @@ namespace ScanReduceReturn
 {
     public partial class Form1 : Form
     {
+        public static string file;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void pbView_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void btnInsertFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Title = "Select A File";
+            openDialog.Filter = "Image Files (*.png;*.jpg)|*.png;*.jpg" + "|" +
+                                "All Files (*.*)|*.*";
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                file = openDialog.FileName;
+            }
+            pbView.Image = Image.FromFile(file);
+            pbView.SizeMode = PictureBoxSizeMode.StretchImage;
         }
     }
 }
